a4ececd0f374fd6122dc12e5d4e56cc520165c3e
hyerim <ax1021@naver.com>
2024-10-31 17:13:41 +0900

    [part1_04] 반복문 for 문 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,21 +1,6 @@
-// switch 문 예시
+// for 문 예시
 void main() {
-  int num = 5;
-
-  switch (num) {
-    case 1:
-      print('Switch $num');
-    case 2:
-      print('Switch $num');
-    case 3:
-      print('Switch $num');
-    case 4:
-      print('Switch $num');
-    case 5:
-      print('Switch $num');
-    case > 100:
-      print('Wow big number');
-    default:
-      print('No4 1~5');
+  for (int i = 0; i < 10; i++) {
+    print('Running For Index $i');
   }
 }
