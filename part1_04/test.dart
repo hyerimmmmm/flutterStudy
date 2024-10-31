8519e4b2afaf800a4a430a02f4e3a0e53254ad8e
hyerim <ax1021@naver.com>
2024-10-31 16:13:29 +0900

    [part1_04] 분기문 else 문 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,21 +1,10 @@
+// if else 문 예시
 void main() {
-  // if 문 예시
-  // // bool 값 활용
-  bool isTrue = true;
+  bool trigger = 10 > 20;
 
-  if (isTrue) {
+  if (trigger) {
     print('True');
-  }
-
-  // 계산식 활용
-  if (10 > 20) {
-    print('True');
-  }
-
-  // String 값 활용
-  String text = 'Hello';
-
-  if (text == 'Hello') {
-    print('True');
+  } else {
+    print('False');
   }
 }
