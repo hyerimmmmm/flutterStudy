181c7af91899424423e68d69886e41f65e8dc5af
hyerim <ax1021@naver.com>
2024-10-31 18:45:23 +0900

    [part1_04] 예외처리문 throw 문 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,12 +1,16 @@
-// on 문 예시
+// throw 문 예시
 void main() {
   int num1 = 10;
 
   try {
+    throw Exception('Unknown Error');
     print(10 ~/ 0);
+
   } on UnsupportedError catch(e, s) {
     print('~/ 해당 연산자는 0으로 나눌 수 없습니다.');
   } on TypeError catch(e, s) {
     print('Null 값이 참조되었습니다.');
+  } catch(e, s) {
+    print('모르는 에러가 발생했습니다.');
   }
 }
