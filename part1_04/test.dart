85e87004ac84dccddf9408b51501713c154e9e5b
hyerim <ax1021@naver.com>
2024-10-31 16:03:49 +0900

    [part1_04] 분기문 if 문 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,3 +1,21 @@
 void main() {
+  // if 문 예시
+  // // bool 값 활용
+  bool isTrue = true;
 
+  if (isTrue) {
+    print('True');
+  }
+
+  // 계산식 활용
+  if (10 > 20) {
+    print('True');
+  }
+
+  // String 값 활용
+  String text = 'Hello';
+
+  if (text == 'Hello') {
+    print('True');
+  }
 }
