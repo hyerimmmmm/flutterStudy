9e141d71fed4a5c2aba9691ae6c8cb77c57548f1
hyerim <ax1021@naver.com>
2024-10-31 18:33:25 +0900

    [part1_04] 예외처리문 on 문 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,13 +1,12 @@
-// try-catch 문 예시
+// on 문 예시
 void main() {
   int num1 = 10;
 
   try {
     print(10 ~/ 0);
-  } catch(error, stack) {
-    print(error);
-    print(stack);
-  } finally {
-    print('예외처리 문이 종료되었습니다');
+  } on UnsupportedError catch(e, s) {
+    print('~/ 해당 연산자는 0으로 나눌 수 없습니다.');
+  } on TypeError catch(e, s) {
+    print('Null 값이 참조되었습니다.');
   }
 }
