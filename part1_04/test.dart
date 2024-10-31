1fe5fab85ac747fee13456bce41334f23a79b098
hyerim <ax1021@naver.com>
2024-10-31 17:45:21 +0900

    [part1_04] 반복문 do-while 문에서 continue 사용하기 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,9 +1,14 @@
-// do-while 문 예시
+// do-while 문에서 continue 사용하기
 void main() {
   int num = 0;
 
   do {
     num++;
+
+    if (num == 4) {
+      continue;
+    }
+
     print('Running Do While $num');
   } while (num < 5);
 }
