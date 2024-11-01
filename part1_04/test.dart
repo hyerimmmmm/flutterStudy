7b9a13bf51ebe4fb12c76974f80fb82775c0424c
hyerim <ax1021@naver.com>
2024-11-01 15:21:18 +0900

    [part1_04] 비동기 키워드 async await Future 실습

--- part1_04/test.dart
+++ part1_04/test.dart
@@ -1,16 +1,11 @@
-// throw 문 예시
+// async await Future 예시
 void main() {
-  int num1 = 10;
-
-  try {
-    throw Exception('Unknown Error');
-    print(10 ~/ 0);
-
-  } on UnsupportedError catch(e, s) {
-    print('~/ 해당 연산자는 0으로 나눌 수 없습니다.');
-  } on TypeError catch(e, s) {
-    print('Null 값이 참조되었습니다.');
-  } catch(e, s) {
-    print('모르는 에러가 발생했습니다.');
+  Future<void> todo(int second) async {
+    await Future.delayed(Duration(seconds: second));
+    print('TODO Done in $second seconds');
   }
+
+  todo(3);
+  todo(1);
+  todo(5);
 }
