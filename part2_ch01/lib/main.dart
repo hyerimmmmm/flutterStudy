1f32d6a01d6c46d815b8712216f893601b656b7b
hyerim <ax1021@naver.com>
2024-11-05 16:55:52 +0900

    [part2_01] 텍스트 추가

--- part2_ch01/lib/main.dart
+++ part2_ch01/lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-
+      body: Text(
+        'hello flutter',
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
     ),
   ));
 }
