ca68d20e2f2538df246787b0da05f02cdf276766
hyerim <ax1021@naver.com>
2024-11-05 17:12:07 +0900

    [part2_01] 클래스 추가 / main 함수 수정

--- part2_ch01/lib/main.dart
+++ part2_ch01/lib/main.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: SafeArea(
-        child: Text(
-          'hello flutter',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
+        body: TestWidget()
     ),
   ));
 }
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Text(
+        'hello flutter',
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
+
+
