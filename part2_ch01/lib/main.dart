86bc53865ab14d306c057632913d3384f71f31ac
hyerim <ax1021@naver.com>
2024-11-06 16:43:46 +0900

    [part2_04] appBar 실습

--- part2_ch01/lib/main.dart
+++ part2_ch01/lib/main.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(icon: Icon(Icons.home), onPressed: () {
+            print('Tab');
+          },),
+          Icon(Icons.play_arrow),
+        ],
+        centerTitle: false,
+        title: Text('This is App bar'),
+      ),
         body: TestWidget()
     ),
   ));
