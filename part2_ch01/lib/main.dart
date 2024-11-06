170dc2b15b3345ca22853fbc7416a29626434088
hyerim <ax1021@naver.com>
2024-11-06 21:57:49 +0900

    [part2_01] Container 실습

--- part2_ch01/lib/main.dart
+++ part2_ch01/lib/main.dart
@@ -19,9 +19,17 @@ class CustomContainer extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 100,
-      color: Color(0xFF9AFFC2),
       padding: EdgeInsets.fromLTRB(10, 15, 20, 25),
       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      decoration: BoxDecoration(
+        color: Color(0xFF9AFFC2),
+        border: Border.all(color: Colors.amber, width: 2, style: BorderStyle.solid),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(color: Colors.black.withOpacity(0.3), offset: Offset(5, 5), blurRadius: 5, spreadRadius: 5),
+          BoxShadow(color: Colors.blue.withOpacity(0.3), offset: Offset(-5, -5), blurRadius: 5, spreadRadius: 5)
+        ],
+      ),
       child: Center(child: Text('Hello Container Hello Container Hello Container Hello Container Hello Container Hello Container Hello Container')),
     );
   }
