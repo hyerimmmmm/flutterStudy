d76d236d33e7291732ca7d9c206d173bf373a19f
hyerim <ax1021@naver.com>
2024-11-07 12:42:32 +0900

    [part2_02] flutter 상하 레이아웃 실습

--- part2_ch02/lib/main.dart
+++ part2_ch02/lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Widget을 상하로 배치하기'),
-          ),
-          body: Body(),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Widget을 상하로 배치하기'),
         ),
+        body: Body(),
       ),
+    ),
   );
 }
 
@@ -19,6 +19,33 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 80,
+            child: Text('Containainer 1'),
+          ),
+          Container(
+            color: Colors.orange,
+            width: 100,
+            height: 80,
+            child: Text('Containainer 2'),
+          ),
+          Container(
+            color: Colors.yellow,
+            width: 100,
+            height: 80,
+            child: Text('Containainer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
