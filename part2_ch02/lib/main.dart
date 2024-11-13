b25fa52fcc4f0a96bb96c73983d2ea45456e0888
hyerim <ax1021@naver.com>
2024-11-13 15:56:29 +0900

    [part2_02] Stack 안에 Container 추가

--- part2_ch02/lib/main.dart
+++ part2_ch02/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Widget을 비율로 배치하기'),
+          title: Text('Widget을 겹겹이 쌓아 올리기'),
         ),
         body: Body(),
       ),
@@ -19,8 +19,29 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      
+    return Stack(
+      children: [
+        Container(
+          width: 500,
+          height: 500,
+          color: Colors.black,
+        ),
+        Container(
+          width: 400,
+          height: 400,
+          color: Colors.red,
+        ),
+        Container(
+          width: 300,
+          height: 300,
+          color: Colors.orange,
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          color: Colors.yellow,
+        ),
+      ],
     );
   }
 }
