659d35aff75b8878d8ac5d92785b4f09e5b66d55
hyerim <ax1021@naver.com>
2024-11-13 19:09:24 +0900

    [part2_03] Stateful과 함께 사용하기 좋은 메서드 1

--- part2_ch03/lib/main.dart
+++ part2_ch03/lib/main.dart
@@ -51,8 +51,15 @@ class ExampleStateful extends StatefulWidget {
 }
 
 class _ExampleStatefulState extends State<ExampleStateful> {
-  int index = 0;
-  
+  late int index;
+
+
+  @override
+  void initState() {
+    super.initState();
+    index = 5;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
