b77c0d50ea3d0888db52a0408484476dd2c70d17
hyerim <ax1021@naver.com>
2024-11-17 23:51:12 +0900

    [part2_04] 제스처디텍터 사용

--- part2_ch04/lib/main.dart
+++ part2_ch04/lib/main.dart
@@ -5,9 +5,9 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Flutter의 Callback'),
+        title: const Text('Flutter의 Callback'),
       ),
-      body: Body(),
+      body: const Body(),
     ),
   ));
 }
@@ -17,6 +17,56 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const TestWidget();
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Count: $value',
+          style: const TextStyle(fontSize: 30),
+        ),
+        const TestButton()
+      ],
+    );
+  }
+}
+
+class TestButton extends StatelessWidget {
+  const TestButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        width: double.infinity,
+        child: GestureDetector(
+          onTap: () => print('Tap!'),
+          child: Center(
+              child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: const Text(
+                    'Up Counter',
+                    style: TextStyle(fontSize: 24),
+                  )
+              )
+          ),
+        )
+    );
   }
 }
