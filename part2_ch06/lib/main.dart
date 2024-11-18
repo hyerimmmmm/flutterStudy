e35431e9da840c7c86270c718549de9489600fae
hyerim <ax1021@naver.com>
2024-11-18 21:38:01 +0900

    [part2_06] 화면 이동하기 실습1

--- part2_ch06/lib/main.dart
+++ part2_ch06/lib/main.dart
@@ -3,20 +3,57 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter에서 화면 이동하기'),
-      ),
-      body: Body(),
-    ),
+      home: HomeWidget()
   ));
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter에서 화면 이동하기'),
+      ),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Person'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() {
+          index = newIndex;
+        }),
+      ),
+    );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return Center(child: Icon(Icons.search, size: 100,),);
+      case 2:
+        return Center(child: Icon(Icons.person, size: 100,),);
+      default:
+        return Center(child: Icon(Icons.home, size: 100,),);
+    }
   }
 }
+
