f94d554f30f3968300355289d01564ce3ead508e
hyerim <ax1021@naver.com>
2024-11-18 21:22:09 +0900

    [part2_06] 프로젝트 생성

---
+++ part2_ch06/android/app/src/main/kotlin/com/example/part2_ch06/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.part2_ch06
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ part2_ch06/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter에서 화면 이동하기'),
+      ),
+      body: Body(),
+    ),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
