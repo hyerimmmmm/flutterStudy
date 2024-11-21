cddc50dcf4c2e8d01e354d3c4ac11aac13ad694e
hyerim <ax1021@naver.com>
2024-11-21 18:50:33 +0900

    [part2] 프로젝트 초기화

--- part2_ch05/lib/main.dart
+++ part2_ch05/lib/main.dart
@@ -2,9 +2,6 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const assetImagePath = 'assets/images';
-const bannerImage = '$assetImagePath/banner.png';
-
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
@@ -21,9 +18,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(bannerImage),
-    );
+    return Placeholder();
   }
 }
 
