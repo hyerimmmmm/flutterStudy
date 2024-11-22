9e2bd268a445f541df76e08af978eff2cb3698ac
hyerim <ax1021@naver.com>
2024-11-22 14:17:27 +0900

    [part2] 상태 변화를 통해 화면 이동하기 실습

--- part2_ch06/lib/main.dart
+++ part2_ch06/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:part2_ch06/Screen/new_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: HomeWidget()
-  ));
+  runApp(MaterialApp(home: HomeWidget()));
 }
 
 class HomeWidget extends StatefulWidget {
@@ -20,7 +17,6 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     index = 0;
   }
@@ -31,14 +27,45 @@ class _HomeWidgetState extends State<HomeWidget> {
       appBar: AppBar(
         title: Text('Flutter에서 화면 이동하기'),
       ),
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const NewPage()));
-            },
-            child: Text('Go to Page')),
-      )
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Person'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() => index = newIndex),
+      ),
     );
   }
-}
 
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return Center(
+          child: Icon(
+            Icons.search,
+            size: 100,
+          ),
+        );
+
+      case 2:
+        return Center(
+          child: Icon(
+            Icons.person,
+            size: 100,
+          ),
+        );
+
+      case 0:
+      default:
+        return Center(
+          child: Icon(
+            Icons.home,
+            size: 100,
+          ),
+        );
+    }
+  }
+}
