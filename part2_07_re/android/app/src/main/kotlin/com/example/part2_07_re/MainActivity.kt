8c284fde859c36eab606f5278f0f17985f0bc291
hyerim <ax1021@naver.com>
2024-11-23 21:48:27 +0900

    [part2] 새프로젝트 추가

---
+++ part2_07_re/android/app/src/main/kotlin/com/example/part2_07_re/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.part2_07_re
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ part2_ch07/android/app/src/main/kotlin/com/example/part2_ch07/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.part2_ch07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
